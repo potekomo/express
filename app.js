98ba73ec42917427899def1721b3202d6a174ceb
ながしま <nagashima.22.527@gmail.com>
2026-06-10 17:54:51 +0900

    add hello page

--- app.js
+++ app.js
@@ -6,21 +6,27 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var helloRouter = require('./routes/hello');
 
+//Express オブジェクトの作成と基本設定 15行目まで
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//関数の組み込み
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//ルート用、エラー用のapp.use 31行目まで
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/hello', helloRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +44,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//.module.exportsの設定
 module.exports = app;
